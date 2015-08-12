853609f39d7cb92fa9e2649600dbbf0ee2976105
Anton Fomenko <a_fomenko@wargaming.net>
2015-08-12 13:35:04 +0300

    General improvements
    1. Remove blank lines in template
    2. Manage resolv.conf on RedHat base OSes with augeas instead if
    template

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,7 @@
 # === Examples
 #
 # You can declare class with resource declaration syntax:
-# class { '::resolv': nameservers => ns.example.com }
+# class { 'resolv': nameservers => ns.example.com }
 # but it's better to do it in hiera and just use indlude directive.
 #
 # === Authors
@@ -22,10 +22,10 @@
 # applewiskey <antony.fomenko@gmail.com>
 #
 class resolv (
-  $nameservers = $resolv::params::nameservers,
-  $search      = $resolv::params::search,
-  $options     = $resolv::params::options
-) inherits resolv::params {
+  $nameservers,
+  $search,
+  $options
+) {
   if $nameservers {
     validate_array($nameservers)
     $nameservers_string = join($nameservers, ' ')
@@ -41,46 +41,47 @@ class resolv (
     $options_string = join($options, ' ')
   }
 
-  $config_path = $::osfamily ? {
-    'Debian' => '/etc/resolvconf/resolv.conf.d/tail',
-    default  => '/etc/resolv.conf',
-  }
+  case $::osfamily {
+    'Solaris': {
+      if $nameservers {
+        exec { "dnsclient.setprop.${nameservers_string}":
+          command => "/usr/sbin/svccfg -s network/dns/client setprop config/nameserver = net_address: \'\"${nameservers_string}\"\'; /usr/sbin/svccfg -s network/dns/client refresh",
+          unless  => "/usr/bin/svcprop -p config/nameserver network/dns/client | grep ${nameservers_string}",
+        }
+      }
+      if $search {
+        exec { "dnsclient.setprop.${search_string}":
+          command => "/usr/sbin/svccfg -s network/dns/client setprop config/search = \'\"${search_string}\"\'; /usr/sbin/svccfg -s network/dns/client refresh",
+          unless  => "/usr/bin/svcprop -p config/search network/dns/client | grep ${search_string}",
+        }
+      }
 
-  $notify = $::osfamily ? {
-    'Debian' => Exec['resovconfupdate'],
-    default  => undef,
-  }
-
-  if $::osfamily == 'Solaris' {
-    if $nameservers {
-      exec { "dnsclient.setprop.${nameservers_string}":
-        command => "/usr/sbin/svccfg -s network/dns/client setprop config/nameserver = net_address: \'\"${nameservers_string}\"\'; /usr/sbin/svccfg -s network/dns/client refresh",
-        unless  => "/usr/bin/svcprop -p config/nameserver network/dns/client | grep ${nameservers_string}",
+      if $options {
+        exec { "dnsclient.setprop.${options_string}":
+          command => "/usr/sbin/svccfg -s network/dns/client setprop config/options = \'\"${options_string}\"\'; /usr/sbin/svccfg -s network/dns/client refresh",
+          unless  => "/usr/bin/svcprop -p config/options network/dns/client | grep ${options_string}",
+        }
       }
     }
-    if $search {
-      exec { "dnsclient.setprop.${search_string}":
-        command => "/usr/sbin/svccfg -s network/dns/client setprop config/search = \'\"${search_string}\"\'; /usr/sbin/svccfg -s network/dns/client refresh",
-        unless  => "/usr/bin/svcprop -p config/search network/dns/client | grep ${search_string}",
+    'Debian': {
+      unless empty($nameservers) {
+        file { '/etc/resolvconf/resolv.conf.d/tail':
+          ensure  => file,
+          content => template('resolv/resolv.conf.erb'),
+        }
+        ~>
+        exec { "resovconfupdate.${nameservers}":
+          command     => '/sbin/resolvconf -u',
+          refreshonly => true,
+        }
       }
     }
-
-    if $options {
-      exec { "dnsclient.setprop.${options_string}":
-        command => "/usr/sbin/svccfg -s network/dns/client setprop config/options = \'\"${options_string}\"\'; /usr/sbin/svccfg -s network/dns/client refresh",
-        unless  => "/usr/bin/svcprop -p config/options network/dns/client | grep ${options_string}",
-      }
-    }
-  } else {
-    unless empty($nameservers) {
-      file { $config_path:
-        ensure  => file,
-        content => template('resolv/resolv.conf.erb'),
-        notify  => $notify,
-      }
-      exec { "resovconfupdate.${nameservers}":
-        command     => '/sbin/resolvconf -u',
-        refreshonly => true,
+    default: {
+      augeas { 'resolv.conf':
+        lens    => 'Resolv.lns',
+        incl    => '/etc/resolv.conf',
+        context => '/files/etc/resolv.conf/options',
+        changes => 'set timeout:1 rotate',
       }
     }
   }
