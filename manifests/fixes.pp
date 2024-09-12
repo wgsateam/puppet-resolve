4e97833ea4a178531cc9e61e00b2fd7a37192c1f
Andrey Burmenko <a_burmenko@wargaming.net>
2024-09-12 11:19:33 +0300

    remove deprecated fixes

--- manifests/fixes.pp
+++ manifests/fixes.pp
@@ -1,21 +1,23 @@
 class resolv::fixes {
-  if $facts['puppet_vardir'] =~ '/opt/puppetlabs' {
-    $_file = '/opt/puppetlabs/puppet/share/augeas/lenses/dist/resolv.aug'
-  } else {
-    $_file = '/usr/share/augeas/lenses/dist/resolv.aug'
-  }
-  if versioncmp($facts['aio_agent_version'], '7.27.0') > 0 {
-    $_opts = ['ip6-dotint','no-ip6-dotint']
-  } else {
-    $_opts = ['ip6-dotint','no-ip6-dotint','trust-ad']
-  }
-  $_opts.each |$o| {
-    file_line { "glibc_2.31_options_${o}_for_resolv.conf":
-      ensure => present,
-      path   => $_file,
-      after  => '.*Build.flag.*rotate.*',
-      line   => "                                     |\"${o}\"",
-      match  => ".*|\"${o}\".*",
+  if versioncmp($facts['aio_agent_version'], '7.32.0') < 0 {
+    if $facts['puppet_vardir'] =~ '/opt/puppetlabs' {
+      $_file = '/opt/puppetlabs/puppet/share/augeas/lenses/dist/resolv.aug'
+    } else {
+      $_file = '/usr/share/augeas/lenses/dist/resolv.aug'
+    }
+    if versioncmp($facts['aio_agent_version'], '7.27.0') > 0 {
+      $_opts = ['ip6-dotint','no-ip6-dotint']
+    } else {
+      $_opts = ['ip6-dotint','no-ip6-dotint','trust-ad']
+    }
+    $_opts.each |$o| {
+      file_line { "glibc_2.31_options_${o}_for_resolv.conf":
+        ensure => present,
+        path   => $_file,
+        after  => '.*Build.flag.*rotate.*',
+        line   => "                                     |\"${o}\"",
+        match  => ".*|\"${o}\".*",
+      }
     }
   }
   # Ensure /etc/resolv.conf is not a symlink
